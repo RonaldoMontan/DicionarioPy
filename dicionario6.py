1582442a85b433af6e70752f23eb9c900005b915
RonaldoMontan <ronaldogithub@gmail.com>
2022-08-03 18:44:30 -0300

    incompleto

--- dicionario6.py
+++ dicionario6.py
@@ -1,59 +1,56 @@
-lista_resumo = list()
-continua = ''
-totalgols = 0
 
-print('\n===Informações de um jogador===\n')
+lista_time = list()
+dicionario_jogador = dict()
+partidas = list()
 
-while continua in 'S':
-    lista_gol = []
-    dicionario = dict()
+while True:
+    dicionario_jogador.clear()#limpar o dicionario para receber novos valores
+    partidas.clear()#limpa as partidas para receber novos valores
+    dicionario_jogador['Nome'] =  str(input('Nome do jogador: '))
+    total_partida = int(input(f'Quantas partidas {dicionario_jogador["Nome"]} jogou: '))
 
 
-    dicionario['Nome jogador'] = str(input('Nome: '))
-    partidas = int(input('Quantas partidas ele jogou: '))
-    
-    #Armazena apenas os gols da partida em uma lista
-    for controle in range(0, partidas):
-        lista_gol.append(int(input(f'Gol da {controle+1}º partida: ')))
-        
-    #lista finalizada de acordo com a quantidade de partidas, vamos salvar no dicionario a lista    
-    dicionario['gol partida'] = lista_gol
-    dicionario['total gols'] = sum(lista_gol)
-    print(sum(lista_gol))
-    lista_resumo.append(dicionario)#Cada indiciduo sera um dicionario armazendaos em uma lista    
-    continua = str(input('Deseja continuar?')).upper()[0]
-    
-cod = 1    
-print('Cod    |   Nome          | Gols       |  Total')
-print('----------------------------------------------')
-for indice, valor in enumerate(lista_resumo):
-    print(f'{indice:>3}', end='')
+    for c in range(0, total_partida):#preenchendo os gols por partida
+        partidas.append(int(input(f'Quantos gols na partida {c+1}º ')))
+
+    dicionario_jogador['gols'] = partidas[:]
+    dicionario_jogador['Total'] = sum(partidas)
+
+    lista_time.append(dicionario_jogador.copy())    #momento em que aliemntamos a lista com o dicionario
+
+    while True: #para forçar a ter a resposta certa
+        continua = str(input('Deseja continuar ?  [S|N]  ')).upper()[0]
+        if continua in 'SN':
+            break
+        print('Resposta invalida Digite S ou N')
+    if continua == 'N':   #preenchido os dados de um jogador, saimos do laço.
+        break        
+print('-=' * 30)
+#exibindo o cabeçalho da tabela
+print('cod  ', end='')
+for i in dicionario_jogador.keys():
+    print(f' {i:<15}', end='')
+print()
+#exibindo formato tabela
+print('-=' * 30)    
+for indice, valor in enumerate(lista_time):
+    print(f'{indice:>4}', end='')
     for dado in valor.values():
-        print(f'{str(dado):<15}         {valor}', end='')
-    print()    
-    cod +=1
+        print(f'{str(dado):<15}', end='')
+    print()  
 
+print('-=' * 40)
 
+while True:
+    busca = 1- (int(input('Digite o codigo do jogador para ver mais detalhes: ')))
 
+    if busca == 999:
+        break
 
+    if busca >= len(lista_time):
+        print(f'ERRO !! Não existe jogador com código {busca}')
+    else:
+        print(f'===Levantamento do Jogador {lista_time[busca]["Nome"]}===')    
 
-
-
-# #são tres tipos de saida:
-# print('      ===Saidas===\n\n')
-# print('=-'*18)
-# print()
-# for chave, valor in dicionario.items():
-#     print(f'O campo  {chave}  tem o  {valor}')  
-# print()
-
-# print('=-'*18)
-# print(f'\n{dicionario}\n')
-
-# print('=-'*18)
-# print()
-# print(f'O jogador {dicionario["Nome jogador"]} jogou {partidas}, acompanhe o resdultado:')
-# for controle in range(0, len(lista_gol)):
-#     print(f'-> Na partida {controle+1}, ele fez {lista_gol[controle]} gols') 
-# print(f'Foi um total de {dicionario["total gols"]} gols')   
-# print()    
+        for indice, gol in enumerate(lista_time[busca]['gols']):
+            print(f'')
